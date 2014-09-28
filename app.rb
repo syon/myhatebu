10909c5ba45cdccd0189f84591d5ca321b29a5d8
syon <syon.xiii@gmail.com>
2014-09-28 18:01:45 +0900

    Feed (Atom)

--- app.rb
+++ app.rb
@@ -4,3 +4,10 @@ get '/' do
   @hotentries = Hotentry.limit(100).order(created_at: :desc)
   slim :index
 end
+
+get '/feed' do
+  @hotentries = Hotentry.limit(20).order(created_at: :desc)
+  @updated = @hotentries.first["created_at"].strftime("%FT%TZ")
+  content_type :"application/atom+xml"
+  slim :feed, :layout => false
+end
